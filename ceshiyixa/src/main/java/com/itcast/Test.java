563f138492527ef2023420ed48a0de21d1a6ec27
mcy <whgmcy@163.com>
2019-07-16 19:47:37 +0800

    提交测试

---
+++ ceshiyixa/src/main/java/com/itcast/Test.java
@@ -0,0 +1,18 @@
+package com.itcast;
+
+public class Test {
+
+    public static void main(String[] args){
+        System.out.println(123);
+        System.out.println(123);
+        System.out.println(123);
+        System.out.println(123);
+        System.out.println(123);
+        System.out.println(123);
+        System.out.println(123);
+        System.out.println(123);
+        System.out.println(123);
+        System.out.println(123);
+        System.out.println(123);
+    }
+}
